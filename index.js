6e0bfef168638ad0001d2f90b97ad5ad50d68d44
jacopobacci <bacci.jaco@gmail.com>
2021-07-09 09:51:07 +0200

    minor style change

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ app.set('view engine', 'ejs');
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/book-face';
 const secret = process.env.SECRET || 'team-four';
 
+
 app.use(
   session({
     store: MongoStore.create({ mongoUrl: dbUrl }),
